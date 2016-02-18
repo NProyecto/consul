176257866279a15a0ea70b78b3c0306f39043de5
kikito <kikito@gmail.com>
2016-02-18 17:51:24 +0100

    adds geozone reference to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216121051) do
+ActiveRecord::Schema.define(version: 20160218164923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -408,10 +408,12 @@ ActiveRecord::Schema.define(version: 20160216121051) do
     t.boolean  "registering_with_oauth",               default: false
     t.string   "locale"
     t.string   "oauth_email"
+    t.integer  "geozone_id"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["geozone_id"], name: "index_users_on_geozone_id", using: :btree
   add_index "users", ["hidden_at"], name: "index_users_on_hidden_at", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
@@ -484,4 +486,5 @@ ActiveRecord::Schema.define(version: 20160216121051) do
   add_foreign_key "moderators", "users"
   add_foreign_key "notifications", "users"
   add_foreign_key "organizations", "users"
+  add_foreign_key "users", "geozones"
 end
